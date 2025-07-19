5be5bfcf86490e5c9b229f87154a92c31f765a69
wuntia-create <badaruwuntia@gmail.com>
2025-07-19 17:41:26 +0000

    calculator

---
+++ main.py
@@ -0,0 +1,8 @@
+#create a function
+def greet():
+    #ask user for the name
+    name= input ("Enter your name:")
+    #Display the name and your greeting
+    print(f"{name}, Nice to meet you!")
+    #function call
+greet()
